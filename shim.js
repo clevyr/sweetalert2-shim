29a065cc17fb5e0e21b9519f69c4291fe397a999
John Pettigrew <john.pettigrew404@gmail.com>
2016-06-03 09:39:48 -0500

    First commit

---
+++ shim.js
@@ -0,0 +1,11 @@
+/*
+ * John Pettigrew
+ * This allows us to use both sweetalert 1 and 2.
+ * Note: This must be loaded before sweetalert 1 and after sweetalert 2.
+ */
+(
+  function(){
+    'use strict';
+    window.sweetalert2 = window.sweetalert;
+  }
+)();
